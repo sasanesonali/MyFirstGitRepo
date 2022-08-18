ceeb5116dce8929661af68f63335c9c46254cf0c
sasanesonali <111496971+sasanesonali@users.noreply.github.com>
2022-08-18 16:51:13 +0530

    Add files via upload

---
+++ src/main/java/demo/test1.java
@@ -0,0 +1,12 @@
+package demo;
+import org.testng.annotations.Test;
+public class test1
+{
+   @Test
+    void testcase1()
+    {
+        System.out.println("Hi...");
+System.out.println("Hi...");
+    }
+
+}
