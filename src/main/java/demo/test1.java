fc4c904205cc9d27d9270d3441280170a0351570
sonalisasane <sasane.sonali@gmail.com>
2022-08-18 17:51:03 +0530

    For930batchcommit

--- src/main/java/demo/test1.java
+++ src/main/java/demo/test1.java
@@ -5,8 +5,8 @@ public class test1
    @Test
     void testcase1()
     {
-        System.out.println("Hi...");
-System.out.println("Hi...");
+        System.out.println("Hi...")
+System.out.println("Hi...")
     }
 
 }
